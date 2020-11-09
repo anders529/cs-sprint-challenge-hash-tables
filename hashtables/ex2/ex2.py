806f97fc35e642d833729d25d17dc3705383f1b5
Aaron Andersen <aaronandersen@zohomail.com>
2020-11-08 20:45:47 -0600

    second test passsing

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -4,11 +4,15 @@ class Ticket:
         self.source = source
         self.destination = destination
 
-
 def reconstruct_trip(tickets, length):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
-
+    route = [None] * length
+    flights = {}
+    for i in tickets:
+        if i.source == 'NONE':
+            first_flight = i
+        flights[i.source] = i.destination
+    route[0] = first_flight.destination
+    for i in range(1, length):
+        source = route[i - 1]
+        route[i] = flights[source]
     return route
